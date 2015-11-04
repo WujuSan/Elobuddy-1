146f3ec3bba38fdf97f6a8586ac6ee232292066b
gmlyra <gabrielmlyra@gmail.com>
2015-11-04 16:37:16 -0200

    Fuk my life e.e

--- One For Week Adc/OneForWeek/Program.cs
+++ One For Week Adc/OneForWeek/Program.cs
@@ -12,8 +12,6 @@ namespace OneForWeek
 {
     class Program
     {
-        public static PluginModel Champion;
-
         static void Main(string[] args)
         {
             Loading.OnLoadingComplete += OnLoadCompleted;
@@ -21,20 +19,17 @@ namespace OneForWeek
 
         private static void OnLoadCompleted(EventArgs args)
         {
-
-            try
+            if(Player.Instance.ChampionName == "Caitlyn")
             {
-                var handle = Activator.CreateInstance(null, "OneForWeek.Plugin.Hero." + ObjectManager.Player.ChampionName);
-                Champion = (PluginModel)handle.Unwrap();
-                Notification.DrawNotification(new NotificationModel(Game.Time, 20f, 1f, ObjectManager.Player.ChampionName + " injected !", Color.DeepSkyBlue));
+                new Caitlyn();
+                Notification.DrawNotification(new NotificationModel(Game.Time, 20f, 1f, Player.Instance.ChampionName + " injected !", Color.DeepSkyBlue));
                 Notification.DrawNotification(new NotificationModel(Game.Time, 20f, 1f, "Addon by: MrArticuno", Color.Purple));
 
                 Igniter.Init();
             }
-            catch (Exception ex)
+            else
             {
-                Console.WriteLine(ex.StackTrace);
-                Notification.DrawNotification(new NotificationModel(Game.Time, 20f, 1f, ObjectManager.Player.ChampionName + " is Not Supported", Color.Red));
+                Notification.DrawNotification(new NotificationModel(Game.Time, 20f, 1f, Player.Instance.ChampionName + " is Not Supported", Color.Red));
             }
         }
     }
