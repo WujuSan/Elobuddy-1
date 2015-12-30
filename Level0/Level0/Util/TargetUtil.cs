2ba3f30baae83852a1a44e32c6843d2549b7992a
gmlyra <gabrielmlyra@gmail.com>
2015-12-30 14:28:17 -0200

    Utils

---
+++ Level0/Level0/Util/TargetUtil.cs
@@ -0,0 +1,51 @@
+﻿using EloBuddy;
+using EloBuddy.SDK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LevelZero.Util
+{
+    static class TargetUtil
+    {
+        //TODO: Popular com buffs que invalidam o alvo (Trindamere, Kayle, Kindred's etc...)
+        public static string[] invalidTargetBuffs = { "" };
+
+        /*
+            Checkage if target is valid.
+        */
+
+        public static bool IsValidTargetUtil(this Obj_AI_Base target)
+        {
+            foreach (var buff in invalidTargetBuffs)
+            {
+                if (target.HasBuff(buff))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static bool IsValidTargetUtil(this Obj_AI_Base target, float range)
+        {
+            foreach (var buff in invalidTargetBuffs)
+            {
+                if (target.HasBuff(buff))
+                {
+                    return false;
+                }
+            }
+
+            if (Player.Instance.Distance(target) > range)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+    }
+}
